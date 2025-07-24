631ee1d6f05b4bc9a91b7b4e0e66585aaecc3400
MYK <matthew@teafortwo>
2025-07-24 13:45:10 +0100

    one thread linux good

--- src/rave_explorer/python/live_encode_decode.py
+++ src/rave_explorer/python/live_encode_decode.py
@@ -14,13 +14,15 @@ def encode_decode(model, input_chunk:np.array):
     # Assumes input_chunk is 1D np.float32 array of length N    
     x = torch.from_numpy(input_chunk).float().unsqueeze(0).unsqueeze(0)  # shape [1, 1, N]
     x = x.expand(1, 2, -1)  # shape [1, 2, N] -> stereo via duplication
+
     # print(f"Input shape is {x.shape}")
     with torch.no_grad():
-        # latent = model.encode(x)
-        # print(f"Latent share {latent.shape}")
+        latent = model.encode(x)
+        print(f"Latent share {latent.shape}")
         # Optionally modify latent here
-        # x_hat = model.decode(latent)
-        x_hat = model.forward(x)
+        latent = latent + np.random.random()
+        x_hat = model.decode(latent)
+        # x_hat = model.forward(x)
         
 
     output = x_hat.squeeze().cpu().numpy()  # shape [N]
@@ -55,70 +57,76 @@ def get_latent_shape(model, device):
     z = torch.randn(B, C, 2048, device=device)
 
 
+def init_jack():
+#  Create the JACK client
+    client = jack.Client("rave_io")
+    # Register a single output port
+    jack_out_L = client.outports.register("outputL")
+    jack_out_R = client.outports.register("outputR")
+    jack_out = client.outports.register("output")
+    jack_in = client.inports.register("input")
 
-device = setup_device()
-model = setup_model(device)
-latent_shape = get_latent_shape(model, device)
-
-# Create the JACK client
-client = jack.Client("noise_sender")
-# Register a single output port
-jack_out = client.outports.register("output")
-jack_in = client.inports.register("input")
-# Auto-connect output to system playback, input to system capture
-
-stop = threading.Event()
-signal.signal(signal.SIGINT, lambda *args: stop.set())
-
-rave_block_size=8192
-
-rave_input_buff = np.zeros(rave_block_size)
-rave_output_buff = np.zeros((2, rave_block_size))
-rave_buff_pos = 0
-
-print(f"Rave buffer pos: {rave_buff_pos}")
-
-@client.set_process_callback
-def process(frames):
-    global rave_buff_pos, rave_input_buff, rave_output_buff
-    in_buf = jack_in.get_array()
-    out_buf = jack_out.get_array()  # NumPy array view of the buffer
-   
-    # print(f"Process: rave pos {rave_buff_pos}")
+    @client.set_process_callback
+    def process(frames):
+        global rave_buff_pos, rave_input_buff, rave_output_buff
+        in_buf = jack_in.get_array()
+        out_buf = jack_out.get_array()  # NumPy array view of the buffer
+        
+        # this bit runs once the script is trying to stop
+        # it prevents any further calls to the torch layer which can cause blocks on exit
+        if stop.is_set():
+            jack_out.get_array()[:] = 0
+            return 0
+        # normal mode: call out to torch 
+        # copy latest into input buffer
+        rave_input_buff[rave_buff_pos : rave_buff_pos + in_buf.size] = in_buf
+        # copy from output 
+        # buf[:] = (np.random.rand(frames) * 2 - 1).astype(np.float32) * 0.1
+        # print(f"Output buffer shape is {rave_output_buff.shape}")
+        out_buf[:] = rave_output_buff[0][rave_buff_pos:rave_buff_pos + in_buf.size]
+        
+        rave_buff_pos = (rave_buff_pos + in_buf.size) % rave_block_size
+        if rave_buff_pos == 0: 
+            # print(f"Pos is {rave_buff_pos}: ready to infer")
+            # ready to process
+            rave_output_buff =   encode_decode(model, rave_input_buff)
     
-    # this bit runs once the script is trying to stop
-    # it prevents any further calls to the torch layer which can cause blocks on exit
-    if stop.is_set():
-        jack_out.get_array()[:] = 0
-        return 0
-    # normal mode: call out to torch 
-    # copy latest into input buffer
-    rave_input_buff[rave_buff_pos : rave_buff_pos + in_buf.size] = in_buf
-    # copy from output 
-    # buf[:] = (np.random.rand(frames) * 2 - 1).astype(np.float32) * 0.1
-    # print(f"Output buffer shape is {rave_output_buff.shape}")
-    out_buf[:] = rave_output_buff[0][rave_buff_pos:rave_buff_pos + in_buf.size]
-    
-    rave_buff_pos = (rave_buff_pos + in_buf.size) % rave_block_size
-    if rave_buff_pos == 0: 
-        # print(f"Pos is {rave_buff_pos}: ready to infer")
-        # ready to process
-        rave_output_buff =   encode_decode(model, rave_input_buff)
-    
+    return client, jack_in, jack_out
 
+def wire_jack(client:jack.Client, jack_in, jack_out):
+    playback_ports = client.get_ports(is_physical=True, is_input=True)
+    capture_ports = client.get_ports(is_physical=True, is_output=True)
+    
+    if playback_ports:
+        jack_out.connect(playback_ports[0])
+        jack_out.connect(playback_ports[1])
+        
+    if capture_ports:
+        jack_in.connect(capture_ports[0])
+        
+
+
+torch.set_num_threads(1)
 
 # Thread-safe signal to stop
 stop = threading.Event()
 signal.signal(signal.SIGINT, lambda *args: stop.set())
 
+device = setup_device()
+model = setup_model(device)
+latent_shape = get_latent_shape(model, device)
+
+rave_block_size=8192
+rave_input_buff = np.zeros(rave_block_size)
+rave_output_buff = np.zeros((2, rave_block_size))
+rave_buff_pos = 0
+
+client, jack_in, jack_out = init_jack()
+
 print(f"Activating client {rave_buff_pos}")
 client.activate()
-playback_ports = client.get_ports(is_physical=True, is_input=True)
-capture_ports = client.get_ports(is_physical=True, is_output=True)
-if playback_ports:
-    jack_out.connect(playback_ports[0])
-if capture_ports:
-    jack_in.connect(capture_ports[0])
+
+wire_jack(client, jack_in, jack_out)
 
 print(f"Sending noise to '{client.name}:output' (fs={client.samplerate} Hz). Press Ctrl+C to quit.")
 
